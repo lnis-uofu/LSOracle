214474c5409ca18f83182487332c7663cfdbbb03
LNIS-Projects <40280375+LNIS-Projects@users.noreply.github.com>
2018-12-14 17:50:54 -0700

    Update travis_run_build.sh

--- travis_run_build.sh
+++ travis_run_build.sh
@@ -1,3 +1,8 @@
+ln -s /usr/bin/gcc-7 /usr/local/bin/gcc
+ln -s /usr/bin/g++-7 /usr/local/bin/g++
+export CC=/usr/bin/gcc-7
+export CXX=/usr/bin/g++-7
+gcc -v && g++ -v && cmake --version
 mkdir build
 cd build
 cmake -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC ..
