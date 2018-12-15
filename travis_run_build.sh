12215eb97d7abb0bdee4db02a471f06485f42f99
LNIS-Projects <40280375+LNIS-Projects@users.noreply.github.com>
2018-12-14 18:53:03 -0700

    Update travis_run_build.sh

--- travis_run_build.sh
+++ travis_run_build.sh
@@ -1,9 +1,4 @@
-ln -s /usr/bin/gcc-7 /usr/local/bin/gcc
-ln -s /usr/bin/g++-7 /usr/local/bin/g++
-export CC=/usr/bin/gcc-7
-export CXX=/usr/bin/g++-7
-gcc -v && g++ -v && cmake --version
 mkdir build
 cd build
-cmake -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC ..
+cmake -DCMAKE_CXX_COMPILER=$COMPILER -DMOCKTURTLE_TEST=ON ..
 make
