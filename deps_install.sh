16f44280eba82fd1fc59db107effa0a3c864ff80
Scott Temple <scott.temple@utah.edu>
2019-05-30 14:37:54 -0600

    vu

--- deps_install.sh
+++ deps_install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [! -d $DEPS_DIR/abc]; then
-    cd $DEPS_DIR
+if [! -d $HOME/deps/abc]; then
+    cd $HOME/deps
     git clone https://github.com/berkeley-abc/abc.git
     cd abc && make ABC_USE_NO_READLINE=1
 else 
     cd $DEPS_DIR/abc
     git pull https://github.com/berkeley-abc/abc.git
-    make ABC_USE_NO_READLINE=1
+    make ABC_USE_NO_READLINE=1 ||:
 fi
