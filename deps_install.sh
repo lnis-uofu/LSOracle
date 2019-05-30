06f5ebeaf3f4c866ebaa4e999aef6aef09a461e5
Scott Temple <scott.temple@utah.edu>
2019-05-30 14:01:32 -0600

    bug fix

--- deps_install.sh
+++ deps_install.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-DEPS_DIR="${HOME}/deps"
-mkdir -p ${DEPS_DIR} && cd ${DEPS_DIR}
-CMAKE_URL="https://cmake.org/files/v3.13/cmake-3.13.0-rc3-Linux-x86_64.tar.gz"
-mkdir -p cmake && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 
 if [! -d "$DEPS_DIR/abc"]; then
     git clone https://github.com/berkeley-abc/abc.git
