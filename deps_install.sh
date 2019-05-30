dfa844fbb6a4336ec12bbdbb8352f03bb890509c
Scott Temple <scott.temple@utah.edu>
2019-05-30 14:23:44 -0600

    more bug fixing

--- deps_install.sh
+++ deps_install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
 if [! -d $DEPS_DIR/abc]; then
+    cd $DEPS_DIR
     git clone https://github.com/berkeley-abc/abc.git
     cd abc && make ABC_USE_NO_READLINE=1
 else 
-    cd abc
+    cd $DEPS_DIR/abc
     git pull https://github.com/berkeley-abc/abc.git
     make ABC_USE_NO_READLINE=1
 fi
