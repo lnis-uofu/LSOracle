1e189b425e6f59857ee3734c98c0192f5d6cf276
maustin8 <max.austin@utah.edu>
2019-11-20 12:29:43 -0700

    Fixed bugs with optimizing and writing sequential networks

--- lib/mockturtle/include/mockturtle/algorithms/refactoring.hpp
+++ lib/mockturtle/include/mockturtle/algorithms/refactoring.hpp
@@ -148,14 +148,16 @@ public:
     } );
 
     ntk.foreach_gate( [&]( auto const& n, auto i ) {
+      // std::cout << "refacotr node = " << n << " is ro " << ntk.is_ro(n) << "\n";
       if ( i >= size )
       {
         return false;
       }
-      if ( ntk.fanout_size( n ) == 0u )
+      if ( ntk.fanout_size( n ) == 0u || ntk.is_ci( n ) )
       {
         return true;
       }
+      // std::cout << "making mffc\n";
       const auto mffc = make_with_stopwatch<mffc_view<Ntk>>( st.time_mffc, ntk, n );
 
       pbar( i, i, _candidates, _estimated_gain );
@@ -165,15 +167,19 @@ public:
         return true;
       }
 
+      // std::cout << "making leaves\n";
       std::vector<signal<Ntk>> leaves( mffc.num_pis() );
       mffc.foreach_pi( [&]( auto const& n, auto j ) {
         leaves[j] = ntk.make_signal( n );
       } );
 
+      // std::cout << "beginning simulation\n";
       default_simulator<kitty::dynamic_truth_table> sim( mffc.num_pis() );
       const auto tt = call_with_stopwatch( st.time_simulation,
                                            [&]() { return simulate<kitty::dynamic_truth_table>( mffc, sim )[0]; } );
-
+      
+      // std::cout << "finished\n";
+      // std::cout << "starting resynthesis\n";
       signal<Ntk> new_f;
       {
         if ( ps.use_dont_cares )
@@ -201,12 +207,13 @@ public:
           refactoring_fn( ntk, tt, leaves.begin(), leaves.end(), [&]( auto const& f ) { new_f = f; return false; } );
         }
       }
-
+      // std::cout << "finished\n";
       if ( n == ntk.get_node( new_f ) )
       {
         return true;
       }
 
+      // std::cout << "calculating gain\n";
       int32_t gain = recursive_deref( n );
       gain -= recursive_ref( ntk.get_node( new_f ) );
 
