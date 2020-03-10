54f72ea27d95a8b6218f8c8cfe914cb4c911f586
wlneto <u1219556@lnissrv4.eng.utah.edu>
2020-03-10 11:45:08 -0600

    adding code to recover AIG from ABC and to run compress2-like optimization inside ABC

--- core/algorithms/output/to_abc.hpp
+++ core/algorithms/output/to_abc.hpp
@@ -7,14 +7,16 @@
 
 #include <ez/direct_iterator.hpp>
 #include <fmt/format.h>
-
 #include <aig/gia/gia.h>
+#include <aig/aig/aig.h>
 #include <aig/gia/giaAig.h>
+#include <base/abc/abc.h>
+#include <base/abci/abc.c>
+#include <base/main/abcapis.h>
 
 /* 
- Creates a simple wrapper with ABC. The mockturtle AIG network is converted into an ABC gia.
- Simple methods are created to iterate over the ABC network. 
- Maybe we should add some map between nodes from mockturtle to ABC.
+  Creates a simple wrapper with ABC. The mockturtle AIG network is converted into an ABC gia.
+  Author: Walter Lau Neto
 */ 
 
 namespace mockturtle
@@ -25,9 +27,49 @@ namespace mockturtle
   {
     public:
 
+      abc_view ()
+      {
+      }
+
       // constructor - derives abc network from mockturtle AIG
       abc_view( mockturtle::aig_network const& ntk )
       {
+        this->to_abc(ntk); 
+
+        std::string fileName = "test.aig"; 
+        abc::Gia_AigerWrite( pNew, fileName.c_str(), 0, 0 );
+        
+        // if(pNew != NULL)
+        //   std::cout << "Conversion done, pointer is not null!\n";
+      }
+
+      void free_manager()
+      {
+        abc::Gia_ManHashStop( pNew );
+      }
+
+      void stats() const 
+      {
+        std::cout << "ABC network stats\n";
+        abc::Gia_ManPrintStats(pNew, 0);
+      }
+
+      void foreach_node() const
+      {
+        abc::Gia_Obj_t* pObj; 
+        for ( auto i = 0; (i < pNew->nObjs) && ((pObj) = abc::Gia_ManObj(pNew, i)); i++ ){
+          if(abc::Gia_ObjIsAnd(pObj))
+          {
+            // to do: iterate over GIA's AND nodes. Just an example 
+          }
+        }
+      }
+
+      void to_abc( mockturtle::aig_network const& ntk )
+      {
+        // gets global frame. It is needed for some optimization procedures
+        pAbc = abc::Abc_FrameGetGlobalFrame();
+
         nOutputs = ntk.num_pos(); 
         nLatches = ntk.num_registers();
         nInputs = ntk.num_pis(); 
@@ -78,7 +120,7 @@ namespace mockturtle
         });
 
         // stops manager's structural hashing    
-        abc::Gia_ManHashStop( pNew );
+        // abc::Gia_ManHashStop( pNew );
         
         // create POs - skipping latches for now 
         vDrivers = abc::Vec_IntAlloc( nLatches + nOutputs );
@@ -96,32 +138,114 @@ namespace mockturtle
         ntk.foreach_po( [&]( auto const& po, auto index ) {
           abc::Gia_ManAppendCo( pNew, abc::Vec_IntEntry(vDrivers, nLatches + index) );
         });
-
-        std::string fileName = "test.aig"; 
-        abc::Gia_AigerWrite( pNew, fileName.c_str(), 0, 0 );
-        
       }
 
-      void stats() const 
+      mockturtle::aig_network from_abc()
       {
-        std::cout << "ABC network stats\n";
-        abc::Gia_ManPrintStats(pNew, 0);
-      }
+        //pNew = pAbc->pGia; 
 
-      void foreach_node() const
-      {
-        abc::Gia_Obj_t* pObj; 
-        for ( auto i = 0; (i < pNew->nObjs) && ((pObj) = abc::Gia_ManObj(pNew, i)); i++ ){
-          if(abc::Gia_ObjIsAnd(pObj))
-          {
-            //
-          }
+        // reads ABC GIA 
+        if(pNew == NULL)
+        {
+          std::cout << "There is no AIG stored in ABC. You must first call the ABC view to deliver an AIG from your current network! \n"; 
+          return; 
         }
+
+        using signal = typename mockturtle::aig_network::signal; 
+
+        // creates a new mockturtle AIG
+        mockturtle::aig_network pMock; 
+        abc::Gia_Obj_t* pObj;
+        int i = 0;
+        unsigned uLit0, uLit1, uLit;
+
+        signal left, right, po; 
+
+        // creates constant
+        pMock.get_constant( false ); 
+
+        for ( i = 0; (i < abc::Gia_ManPiNum(pNew)) && ((pObj) = abc::Gia_ManCi(pNew, i)); i++ )
+        {
+          // creates PIs with default name. TO DO: registers 
+          pMock.create_pi();
+        }
+
+        for ( i = 0; (i < pNew->nObjs) && ((pObj) = abc::Gia_ManObj(pNew, i)); i++ )      if ( !abc::Gia_ObjIsAnd(pObj) ) {} else
+        {
+          // create AND nodes 
+          uLit  = abc::Abc_Var2Lit( i, 0 );
+          uLit0 = abc::Gia_ObjFaninLit0( pObj, i );
+          uLit1 = abc::Gia_ObjFaninLit1( pObj, i );
+          left = pMock.child_to_signal( uLit0 );
+          right = pMock.child_to_signal( uLit1 );
+
+          if( uLit0 & 1 )
+          {
+            left = pMock.create_not( left ); 
+          }
+          
+          if( uLit1 & 1 )
+          {
+            right = pMock.create_not( right ); 
+          }
+
+          pMock.create_and( left, right );
+        }
+
+        for ( i = 0; (i < abc::Gia_ManPoNum(pNew)) && ((pObj) = abc::Gia_ManCo(pNew, i)); i++ )
+        {
+          if(!abc::Gia_ObjIsCo(abc::Gia_ObjChild0(pObj)))
+          {
+            //uLit  = abc::Abc_Var2Lit( i, 0 );
+            uLit = abc::Gia_ObjFaninLit0( pObj, Gia_ObjId(pNew,pObj) );
+          }
+
+          else if (!abc::Gia_ObjIsCo(abc::Gia_ObjChild1(pObj)))
+          {
+            uLit = abc::Gia_ObjFaninLit1( pObj, Gia_ObjId(pNew,pObj) );
+          }
+          
+          po = pMock.child_to_signal( uLit );
+
+          if( uLit & 1 )
+          {
+            po = pMock.create_not( po );
+          }
+
+          pMock.create_po( po );
+
+        }
+        return pMock; 
+      }
+
+      void dc2()
+      {
+        if(pNew == NULL){
+          printf("There is no stored AIG\n");
+          return; 
+        }
+
+        abc::Gia_Man_t *pTmp; 
+
+        printf("starting optimization\n");
+        pTmp = pNew; 
+        pNew = abc::Gia_ManCompress2( pTmp, 1, 0 );
+        printf("done with optimization\n");
+        abc::Gia_ManPrintStats(pNew, 0);
+        abc::Abc_FrameUpdateGia( pAbc, pNew );
+      }
+
+      bool abc_has_aig()
+      {
+        if(pNew == NULL)
+          return false;
+        return true; 
       }
 
       private: 
         // variables to use with ABC
         abc::Gia_Man_t *pNew;
+        abc::Abc_Frame_t *pAbc; 
         abc::Vec_Int_t * vLits = NULL;
 
         int nTotal, nInputs, nOutputs, nLatches, nAnds, i;
--- core/commands/testing/ntk_to_abc.hpp
+++ core/commands/testing/ntk_to_abc.hpp
@@ -8,6 +8,9 @@
 
 namespace alice
 {
+
+  mockturtle::abc_view abc; 
+
   class ntk_to_abc_command : public alice::command{
 
     public:
@@ -20,7 +23,7 @@ namespace alice
         if(!store<aig_ntk>().empty()){
           mockturtle::aig_network ntk = *store<aig_ntk>().current();
           //mockturtle::ntk_to_abc( ntk ); 
-          mockturtle::abc_view abc{ntk}; 
+          abc.to_abc(ntk);
           abc.stats();
           abc.foreach_node(); 
         }
@@ -29,7 +32,42 @@ namespace alice
         }
       }
     private:
-    };
+  };
 
-  ALICE_ADD_COMMAND(ntk_to_abc, "Testing");
+    ALICE_ADD_COMMAND(ntk_to_abc, "Testing");
+
+  class from_abc_command : public alice::command{
+
+    public:
+      explicit from_abc_command( const environment::ptr& env )
+        : command( env, "Convert AIG into ABC GIA data structure" ){
+      }
+
+    protected:
+      void execute(){
+        if(!store<aig_ntk>().empty()){
+          
+          if( abc.abc_has_aig() )
+          {
+            std::cout << "There is an AIG in ABC\n";
+          }
+          mockturtle::aig_network ntk = abc.from_abc(); 
+          printf("ntk size: %d, and nodes: %d, num_pis: %d, and num pos: %d\n", ntk.size(), ntk.num_gates(), ntk.num_pis(), ntk.num_pos());
+          abc.dc2();
+          auto ntk2 = abc.from_abc(); 
+          printf("new ntk size: %d, and nodes: %d, num_pis: %d, and num pos: %d\n", ntk2.size(), ntk2.num_gates(), ntk2.num_pis(), ntk2.num_pos());
+
+          abc.free_manager();
+          //mockturtle::ntk_to_abc( ntk ); 
+          //abc.stats();
+          //abc.foreach_node(); 
+        }
+        else{
+          std::cout << "No AIG stored\n";
+        }
+      }
+    private:
+  };
+
+  ALICE_ADD_COMMAND(from_abc, "Testing");
 }
