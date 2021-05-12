8fa2b1177954e84c1d945c94198d2d101e365d1e
Ashton Snelgrove <ashton.snelgrove@utah.edu>
2021-05-12 13:24:51 -0600

    Add copy of core boost from archive 1.76.0

---
+++ lib/kahypar/build_clang/tools/CMakeFiles/BookshelfToHgr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BookshelfToHgr"
+  "BookshelfToHgr.pdb"
+  "CMakeFiles/BookshelfToHgr.dir/bookshelf_to_hgr_converter.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BookshelfToHgr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lib/kahypar/build_clang/tools/CMakeFiles/RepeatsToHgr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RepeatsToHgr.dir/repeats_to_hgr_converter.cc.o"
+  "RepeatsToHgr"
+  "RepeatsToHgr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RepeatsToHgr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
