bbf9fb906c835af1a76be833c7ac00cd02f3a409
Scott Temple <scott.temple@utah.edu>
2021-08-26 00:32:30 -0600

    Added real XAG script

---
+++ core/algorithms/optimization/xag_script.hpp
@@ -0,0 +1,70 @@
+/* LSOracle: A learning based Oracle for Logic Synthesis
+
+ * MIT License
+ * Copyright 2019 Laboratory for Nano Integrated Systems (LNIS)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <kitty/kitty.hpp>
+#include <mockturtle/mockturtle.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fmt/format.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace oracle
+{
+
+class xag_script
+{
+public:
+    mockturtle::xag_network run(mockturtle::xag_network &xag)
+    {
+        mockturtle::sop_rebalancing<mockturtle::xag_network> balfn;
+        mockturtle::xag_npn_resynthesis<mockturtle::xag_network> resyn;
+        mockturtle::refactoring_params rp;
+        mockturtle::balancing_params bs;
+        mockturtle::cut_rewriting_params ps;
+        ps.cut_enumeration_ps.cut_size = 4u;
+        bs.cut_enumeration_ps.cut_size = 4u;
+        rp.allow_zero_gain = false;
+        rp.max_pis = 4;
+        
+        mockturtle::cut_rewriting(xag, resyn, ps); //rw
+        xag = mockturtle::cleanup_dangling(xag);
+        mockturtle::cut_rewriting(xag, resyn, ps); //rw
+        xag = mockturtle::cleanup_dangling(xag);
+
+        xag = mockturtle::balancing(xag, {balfn}, bs); //b
+        xag = mockturtle::cleanup_dangling(xag);
+        mockturtle::refactoring(xag, resyn, rp); //rf
+        xag = mockturtle::cleanup_dangling(xag);
+
+        xag = mockturtle::xag_constant_fanin_optimization(xag); //find XORs
+
+        return xag;
+    }
+};
+}
--- core/lsoracle.cpp
+++ core/lsoracle.cpp
@@ -60,6 +60,8 @@
 #include "algorithms/optimization/optimization.hpp"
 #include "algorithms/optimization/optimization_test.hpp"
 #include "algorithms/optimization/xmg_script.hpp"
+#include "algorithms/optimization/xag_script.hpp"
+
 //#include "algorithms/asic_mapping/techmapping.hpp"
 //#include "algorithms/output/mapped_verilog.hpp"
 
@@ -120,6 +122,8 @@
 #include "commands/optimization/refactor.hpp"
 #include "commands/optimization/oracle.hpp"
 #include "commands/optimization/xmgscript.hpp"
+#include "commands/optimization/xagscript.hpp"
+
 
 /* these were commented out in previous master */
 // #include "commands/optimization/depth_partition.hpp"
