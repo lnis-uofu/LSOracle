e81149d9ddb33cd23832670833c5744ef14b30c4
Scott Temple <scott.temple@utah.edu>
2022-02-24 13:29:23 -0700

    New optimization recipes (#66)
    * added XMG store and I/O
    
    * Implemented XMG depth optimization
    
    * Added real XAG script
    
    * added area oriented AIG script (strategy 3), fixed typos
    
    * added FRAIGing/FRMIGing to several scripts
    
    * improved AIG recipes, fixed bug in aig_script3
    
    * added names views to recipes, fixed bug in aig_script5

--- core/commands/input/read_bench.hpp
+++ core/commands/input/read_bench.hpp
@@ -103,6 +103,27 @@ protected:
 
                 filename.erase(filename.end() - 6, filename.end());
                 named_dest.set_network_name(filename);
+            } else if (is_set("xmg")) {
+                mockturtle::klut_network klut_ntk;
+                mockturtle::names_view<mockturtle::klut_network> klut_name_view{klut_ntk};
+                auto const result = lorina::read_bench(filename,
+                                                       mockturtle::bench_reader(klut_name_view));
+
+                if (result != lorina::return_code::success)
+                    env->err() << "parsing failed\n";
+
+                mockturtle::xmg_npn_resynthesis resyn;
+
+                mockturtle::xmg_network ntk;
+                mockturtle::names_view<mockturtle::xmg_network>named_dest(ntk);
+
+                mockturtle::node_resynthesis(named_dest, klut_name_view, resyn);
+
+                store<xmg_ntk>().extend() = std::make_shared<xmg_names>(named_dest);
+                env->out() << "XMG network stored\n";
+
+                filename.erase(filename.end() - 6, filename.end());
+                named_dest.set_network_name(filename);
             } else if (is_set("aig")) {
 
                 mockturtle::klut_network ntk;
---
+++ core/commands/optimization/xagscript.hpp
@@ -0,0 +1,77 @@
+/* LSOracle: A learning based Oracle for Logic Synthesis
+
+ * MIT License
+ * Copyright 2021 Laboratory for Nano Integrated Systems (LNIS)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <alice/alice.hpp>
+
+#include <mockturtle/mockturtle.hpp>
+
+#include <stdio.h>
+#include <fstream>
+
+#include <sys/stat.h>
+#include <stdlib.h>
+
+namespace alice
+{
+class xagscript_command : public alice::command
+{
+
+public:
+    explicit xagscript_command(const environment::ptr &env)
+        : command(env, "Perform XAG based optimization")
+    {
+
+        //opts.add_option("--strategy", strategy, "Optimization strategy [0-4]");
+    }
+
+protected:
+    void execute()
+    {
+        if (!store<xag_ntk>().empty()) {
+            auto &opt = *store<xag_ntk>().current();
+            mockturtle::depth_view xag_depth{opt};
+            env->out() << "XAG logic depth " << xag_depth.depth() << " nodes " <<
+                       opt.num_gates() << std::endl;
+            auto start = std::chrono::high_resolution_clock::now();
+            oracle::xag_script xagopt;
+            opt = xagopt.run(opt);
+
+            mockturtle::depth_view new_xag_depth{opt};
+            env->out() << "Final ntk size = " << opt.num_gates() << " and depth = " << new_xag_depth.depth() << "\n";
+            env->out() << "Area Delay Product = " << opt.num_gates() * new_xag_depth.depth() << "\n";
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
+                            (stop - start);
+            env->out() << "Full Optimization: " << duration.count() << "ms\n";
+            env->out() << "Finished optimization\n";
+        } else {
+            env->err() << "There is not an xag network stored.\n";
+        }
+    }
+}; //class
+ALICE_ADD_COMMAND(xagscript, "Optimization");
+} //namespace alice
+
